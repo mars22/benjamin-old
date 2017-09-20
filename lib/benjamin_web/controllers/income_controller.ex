fc4eecd1eab8233e3575cfbd74d8a49c598c5062
marcin <mslowinski79@gmail.com>
2017-09-20 23:00:24 +0200

    Added plug to icome congtroller that assign fetch balance from db base on balace_id and assign it to connection

--- lib/benjamin_web/controllers/income_controller.ex
+++ lib/benjamin_web/controllers/income_controller.ex
@@ -4,14 +4,16 @@ defmodule BenjaminWeb.IncomeController do
   alias Benjamin.Finanses
   alias Benjamin.Finanses.Income
 
+  plug :assign_balance
+
   def index(conn, %{"balance_id" => balance_id}) do
     incomes = Finanses.list_incomes()
-    render(conn, "index.html", [incomes: incomes, balance_id: balance_id])
+    render(conn, "index.html", incomes: incomes)
   end
 
   def new(conn, %{"balance_id" => balance_id}) do
     changeset = Finanses.change_income(%Income{})
-    render(conn, "new.html", changeset: changeset, balance_id: balance_id)
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"income" => income_params, "balance_id" => balance_id}) do
@@ -22,7 +24,17 @@ defmodule BenjaminWeb.IncomeController do
         |> put_flash(:info, "Income created successfully.")
         |> redirect(to: balance_path(conn, :show, income.balance_id))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset, balance_id: balance_id)
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp assign_balance(conn, _opts) do
+    case conn.params do
+      %{"balance_id" => balance_id} ->
+        balance = Finanses.get_balance!(balance_id)
+        assign(conn, :balance, balance)
+      _ ->
+        conn
     end
   end
 end
