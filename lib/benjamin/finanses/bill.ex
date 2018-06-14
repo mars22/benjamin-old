fdffb2d765044d6a47cc88f6ae316ead7be1763e
Marcin Slowinski <mslowinski79@gmail.com>
2018-06-14 07:59:03 +0200

    simplify bill copy function

--- lib/benjamin/finanses/bill.ex
+++ lib/benjamin/finanses/bill.ex
@@ -4,15 +4,14 @@ defmodule Benjamin.Finanses.Bill do
   alias Benjamin.Accounts.Account
   alias Benjamin.Finanses.{Bill, BillCategory, Budget}
 
-
   schema "bills" do
-    field :amount, :decimal, default: Decimal.new(0)
-    field :planned_amount, :decimal
-    field :description, :string, default: ""
-    field :paid_at, :date, default: nil
-    belongs_to :budget, Budget
-    belongs_to :category, BillCategory, source: :category_id
-    belongs_to :account, Account
+    field(:amount, :decimal, default: Decimal.new(0))
+    field(:planned_amount, :decimal)
+    field(:description, :string, default: "")
+    field(:paid_at, :date, default: nil)
+    belongs_to(:budget, Budget)
+    belongs_to(:category, BillCategory, source: :category_id)
+    belongs_to(:account, Account)
 
     timestamps()
   end
@@ -20,15 +19,29 @@ defmodule Benjamin.Finanses.Bill do
   @doc false
   def changeset(%Bill{} = bill, attrs) do
     bill
-    |> cast(attrs, [:category_id, :budget_id, :amount, :planned_amount, :description, :paid_at, :account_id])
+    |> cast(attrs, [
+      :category_id,
+      :budget_id,
+      :amount,
+      :planned_amount,
+      :description,
+      :paid_at,
+      :account_id
+    ])
     |> validate_required([:category_id, :budget_id, :planned_amount])
     |> validate_number(:planned_amount, greater_than_or_equal_to: 0)
     |> unique_constraint(:category_id, name: :bills_budget_id_category_id_index)
   end
 
   def copy(%Bill{} = bill) do
-    extra = %{inserted_at: Ecto.DateTime.utc, updated_at: Ecto.DateTime.utc}
-    copied = %{account_id: bill.account_id, budget_id: bill.budget_id, planned_amount: bill.amount, amount: Decimal.new(0) , category_id: bill.category_id}
-    Map.merge(copied, extra)
+    %{
+      account_id: bill.account_id,
+      budget_id: bill.budget_id,
+      planned_amount: bill.amount,
+      amount: Decimal.new(0),
+      category_id: bill.category_id,
+      inserted_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    }
   end
 end
