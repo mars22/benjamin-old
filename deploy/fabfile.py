e2dc82d28a0253cc3289ff02d84f1f81c33709b0
Marcin Slowinski <mslowinski79@gmail.com>
2018-11-13 18:55:23 +0100

    Updated README

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -4,45 +4,33 @@ from fabric import api
 from fabric.contrib.files import exists
 
 api.env.use_ssh_config = True
-api.env.roledefs = {
-    'PROD': ['aws'],
-}
+api.env.roledefs = {"PROD": ["aws"]}
 
-api.env.project_src = '/home/ubuntu/app/benjamin/'
-api.env.git_url = 'git@bitbucket.org:mslowinski/benjamin.git'
-api.env.prompts = {
-    'Are you sure you want to do this?': 'yes',
-}
+api.env.project_src = "/home/ubuntu/app/benjamin/"
+api.env.git_url = "<<GIT_REPO_URL>>"
+api.env.prompts = {"Are you sure you want to do this?": "yes"}
 
 
-@api.roles('PROD')
+@api.roles("PROD")
 def update():
-    with contextlib.nested(
-        api.cd('%(project_src)s' % api.env),
-    ):
-        api.run('git pull')
-        api.run('MIX_ENV=prod mix deps.get')
-        with contextlib.nested(
-            api.cd('%(project_src)s/assets' % api.env),
-        ):
-          api.run('npm run deploy')
+    with contextlib.nested(api.cd("%(project_src)s" % api.env)):
+        api.run("git pull")
+        api.run("MIX_ENV=prod mix deps.get")
+        with contextlib.nested(api.cd("%(project_src)s/assets" % api.env)):
+            api.run("npm run deploy")
 
-        api.run('MIX_ENV=prod mix compile --force')
-        api.run('MIX_ENV=prod mix phx.digest')
-        api.sudo('systemctl restart benjamin.service')
+        api.run("MIX_ENV=prod mix compile --force")
+        api.run("MIX_ENV=prod mix phx.digest")
+        api.sudo("systemctl restart benjamin.service")
 
 
-@api.roles('PROD')
+@api.roles("PROD")
 def migrate():
-    with contextlib.nested(
-        api.cd('%(project_src)s' % api.env),
-    ):
-        api.run('MIX_ENV=prod mix ecto.migrate')
+    with contextlib.nested(api.cd("%(project_src)s" % api.env)):
+        api.run("MIX_ENV=prod mix ecto.migrate")
 
 
-@api.roles('PROD')
+@api.roles("PROD")
 def rollback():
-    with contextlib.nested(
-        api.cd('%(project_src)s' % api.env),
-    ):
-        api.run('MIX_ENV=prod mix ecto.rollback')
+    with contextlib.nested(api.cd("%(project_src)s" % api.env)):
+        api.run("MIX_ENV=prod mix ecto.rollback")
